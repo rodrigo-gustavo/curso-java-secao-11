c82ba3ee5866341a95c0aae712465530eff238f5
Rodrigo Gustavo <2338.gustavo@gmail.com>
2021-06-07 22:35:12 -0300

    Salvando aula sobre datas

---
+++ src/ManipulandoData.java
@@ -0,0 +1,34 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+public class ManipulandoData {
+
+	public static void main(String[] args) throws ParseException {
+		
+		 SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		 
+		 Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		 
+		 System.out.println(sdf.format(d));
+		 
+		 Calendar cal = Calendar.getInstance();//Intanciando Calendar
+		 cal.setTime(d);
+		 cal.add(Calendar.HOUR_OF_DAY, 4);//adiciona dia, hora, minuto e etc, ao calendário
+		 d = cal.getTime(); //atualizando a variavel "d" com a nova data
+		 
+		 int minutes = cal.get(Calendar.MINUTE);
+		 int month = 1 + cal.get(Calendar.MONTH);
+		 int second = cal.get(Calendar.SECOND);
+		 
+		 System.out.println("Minutes: " + minutes);
+		 System.out.println("Month: " + month);
+		 System.out.println("Second: " + second);
+		 
+		 
+
+	}
+
+}
